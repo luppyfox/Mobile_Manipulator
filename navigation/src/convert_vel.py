dde8140c2221b2136c63bdacb45075da5287fd77
phat <luppy.fox@gmail.com>
2024-01-17 10:53:25 -0800

    update launch file odom.py and convert_vel.py to navigation package

---
+++ navigation/src/convert_vel.py
@@ -0,0 +1,56 @@
+#! /usr/bin/env python3
+from math import pi
+import rospy
+from geometry_msgs.msg import Twist
+from std_msgs.msg import Float32
+
+
+class Diff_drive_kinematics():
+    def __init__(self):
+        self.sub = rospy.Subscriber('/cmd_vel', Twist, self.callback)
+        self.right_wheel_pub = rospy.Publisher(
+            '/control_right_wheel/command', Float32, queue_size=1)
+        self.left_wheel_pub = rospy.Publisher(
+            '/control_left_wheel/command', Float32, queue_size=1)
+        self.cmd_vel = Twist()
+        self.vl = Float32()
+        self.vr = Float32()
+        self.rpmL = Float32()
+        self.rpmR = Float32()
+        self.rate = rospy.Rate(1)
+
+    def callback(self, msg):
+        self.cmd_vel = msg
+
+    def pub_wheel_vels(self):
+        while not rospy.is_shutdown():
+            self.convert_vels()
+            self.left_wheel_pub.publish(self.rpmL)
+            self.right_wheel_pub.publish(self.rpmR)
+            self.rate.sleep()
+
+    def convert_vels(self):
+        L = 0.309  # //L is distance between wheels (wheel base)
+        R = 0.0421  # //R is RADIUS     Diameter is 0.12
+        self.vl = self.cmd_vel.linear.x - (self.cmd_vel.angular.z*(L/2))
+        self.vr = self.cmd_vel.linear.x + (self.cmd_vel.angular.z*(L/2))
+        self.rpmL = (self.vl*60)/(2*pi*R)
+        self.rpmR = (self.vr*60)/(2*pi*R)
+
+        if self.rpmL >= 8:
+            self.rpmL = 8
+
+        if self.rpmR >= 8:
+            self.rpmR = 8
+
+        if self.rpmL <= -8:
+            self.rpmL = -8
+
+        if self.rpmR <= -8:
+            self.rpmR = -8
+
+
+if __name__ == '__main__':
+    rospy.init_node('convert_vel_node', anonymous=True)
+    diffdrive_object = Diff_drive_kinematics()
+    diffdrive_object.pub_wheel_vels()
---
+++ navigation/src/odom.py
@@ -0,0 +1,211 @@
+#! /usr/bin/env python3
+import rospy
+from math import pi, sin, cos
+from geometry_msgs.msg import Twist, Pose, Point, Quaternion, Vector3
+from nav_msgs.msg import Odometry
+from std_msgs.msg import Int64
+import tf
+from tf.broadcaster import TransformBroadcaster
+
+class OdometryClass:
+    def __init__(self):
+            self.enc_l_sub = rospy.Subscriber('/enc_L', Int64, self.callback_L)
+            self.enc_r_sub = rospy.Subscriber('/enc_R', Int64, self.callback_R)
+            self.odom_pub = rospy.Publisher('/odom', Odometry, queue_size = 1)
+            self.odom = Odometry()
+            self.rate = rospy.Rate(200)
+            self.odom_broadcaster = TransformBroadcaster()
+            self.lastL_ticks = 0
+            self.lastR_ticks = 0
+            self.currentL_ticks = 0
+            self.currentR_ticks = 0
+            self.current_time = rospy.Time.now()
+            self.last_time = rospy.Time.now()
+            self.L = 0.309 #distance between robot wheels
+            self.R = 0.0421 #radius of wheel unit m
+            self.N = 20181 #total pulse per round
+            self.x = 0
+            self.y = 0
+            self.theta = 0
+            self.updatePose()
+
+    def callback_L(self, msg):
+        self.currentL_ticks = msg.data
+        if self.lastL_ticks == 0:
+            self.lastL_ticks == msg.data
+
+    def callback_R(self, msg):
+        self.currentR_ticks = msg.data
+        if  self.lastR_ticks == 0:
+            self.lastR_ticks = msg.data
+        
+    def updatePose(self):
+        while not rospy.is_shutdown():
+            delta_l = self.currentL_ticks - self.lastL_ticks
+            delta_r = self.currentR_ticks - self.lastR_ticks
+            d_l = 2 * pi * self.R * (delta_l / self.N)
+            d_r = 2 * pi * self.R * (delta_r / self.N)
+            
+            self.lastL_ticks = self.currentL_ticks
+            self.lastR_ticks = self.currentR_ticks
+            
+            self.current_time = rospy.Time.now()
+            dt = (self.current_time - self.last_time).to_sec()
+            
+            th = (d_r - d_l) / self.L
+            dc = (d_r + d_l) / 2
+            v = dc / dt
+            w = th / dt
+
+            delta_x = dc * cos(self.theta)
+            delta_y = dc * sin(self.theta)
+            delta_th = th
+
+            self.x += delta_x
+            self.y += delta_y
+            self.theta += delta_th
+
+            odom_quat = tf.transformations.quaternion_from_euler(0, 0, self.theta)
+
+            self.odom_broadcaster.sendTransform((self.x, self.y, 0), odom_quat, self.current_time, "base_footprint", "odom")
+            odom = Odometry()
+            odom.header.stamp = self.current_time
+            odom.header.frame_id = "odom"
+
+            odom.pose.pose = Pose(Point(self.x, self.y, 0), Quaternion(*odom_quat))
+
+            odom.child_frame_id = "base_footprint"
+            odom.twist.twist = Twist(Vector3(v, 0, 0), Vector3(0, 0, w))
+            
+            self.odom_pub.publish(odom)
+
+            self.last_time = self.current_time
+            self.rate.sleep()
+
+if __name__ == "__main__":
+    rospy.init_node('pub_odom')
+    oc = OdometryClass() 
+    rospy.spin()
+
+            
+
+
+
+# #! /usr/bin/env python3
+# import rospy
+# from math import pi, sin, cos
+# from geometry_msgs.msg import Twist, Pose, Point, Quaternion, Vector3
+# from std_msgs.msg import Int64
+# from nav_msgs.msg import Odometry
+
+# import tf
+# from tf.broadcaster import TransformBroadcaster
+
+
+# class OdometryClass:
+
+#     def __init__(self):
+#         self.left_ticks_sub = rospy.Subscriber(
+#             '/Enc_L', Int64, self.getTicks_L)
+#         self.right_ticks_sub = rospy.Subscriber(
+#             '/Enc_R', Int64, self.getTicks_R)
+
+#         self.odom_pub = rospy.Publisher('/odom', Odometry, queue_size=1)
+#         self.odom_broadcaster = TransformBroadcaster()
+#         self.odom = Odometry()
+#         self.rate = rospy.Rate(200)
+#         self.lastLeftTicks = 0
+#         self.lastRightTicks = 0
+#         self.currentLeftTicks = 0
+#         self.currentRightTicks = 0
+#         self.current_time = rospy.Time.now()
+#         self.last_time = rospy.Time.now()
+
+#         self.L = 0.308
+#         self.R = 0.0421
+#         self.N = 20181
+
+#         self.x = 0.0
+#         self.y = 0.0
+#         self.th = 0.0
+
+#         rospy.loginfo("Odom Start!!!")
+#         self.updatePose()
+
+#     def getTicks_L(self, msg):
+#         self.currentLeftTicks = msg.data
+
+#         if self.lastLeftTicks == 0:
+#             self.lastLeftTicks = msg.data
+
+#     def getTicks_R(self, msg):
+#         self.currentRightTicks = msg.data
+
+#         if self.lastRightTicks == 0:
+#             self.lastRightTicks = msg.data
+
+#     def updatePose(self):
+
+#         while not rospy.is_shutdown():
+
+#             delta_l = self.currentLeftTicks - self.lastLeftTicks
+#             delta_r = self.currentRightTicks - self.lastRightTicks
+#             d_l = 2 * pi * self.R * (delta_l / self.N)
+#             d_r = 2 * pi * self.R * (delta_r / self.N)
+
+#             self.lastLeftTicks = self.currentLeftTicks
+#             self.lastRightTicks = self.currentRightTicks
+#             self.current_time = rospy.Time.now()
+#             dt = (self.current_time - self.last_time).to_sec()
+
+#             dc = (d_r + d_l)/2
+#             th = (d_r - d_l)/self.L
+
+#             v = dc / dt
+#             w = th / dt
+
+#             # compute odometry in a typical way given the velocities of the robot
+#             delta_x = dc * cos(self.th)
+#             delta_y = dc * sin(self.th)
+#             delta_th = th
+
+#             self.x += delta_x
+#             self.y += delta_y
+#             self.th += delta_th
+
+#             # since all odometry is 6DOF we'll need a quaternion created from yaw
+#             odom_quat = tf.transformations.quaternion_from_euler(0, 0, self.th)
+
+#             # first, we'll publish the transform over tf
+#             # self.odom_broadcaster.sendTransform(
+#             #    (self.x, self.y, 0),
+#             #    odom_quat,
+#             #     self.current_time,
+#             #     "base_footprint",
+#             #     "odom"
+#             # )
+
+#             # next, we'll publish the odometry message over ROS
+#             odom = Odometry()
+#             odom.header.stamp = self.current_time
+#             odom.header.frame_id = "odom"
+
+#             # set the position
+#             odom.pose.pose = Pose(
+#                 Point(self.x, self.y, 0.), Quaternion(*odom_quat))
+
+#             # set the velocity
+#             odom.child_frame_id = "base_footprint"
+#             odom.twist.twist = Twist(Vector3(v, 0, 0), Vector3(0, 0, w))
+
+#             # publish the message
+#             self.odom_pub.publish(odom)
+
+#             self.last_time = self.current_time
+#             self.rate.sleep()
+
+
+# if __name__ == "__main__":
+#     rospy.init_node('odom_node')
+#     oc = OdometryClass()
+#     rospy.spin()
