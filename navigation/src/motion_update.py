2c85cb8ee6f6591afaea2d01f0d3a044534cd9a3
LuppyFox <luppy.fox@gmail.com>
2024-02-13 06:14:44 -0800

    Create manual publish goal position to test robot

--- navigation/src/motion_update.py
+++ navigation/src/motion_update.py
@@ -5,14 +5,19 @@ from std_srvs.srv import Empty
 def call_service():
     rate = rospy.Rate(0.2) # 1 times per 5 second
     try:
-        rospy.wait_for_service('/request_nomotion_update')
+        rospy.wait_for_service('request_nomotion_update')
         reset_service = rospy.ServiceProxy('request_nomotion_update', Empty)
+        rospy.loginfo("test2")
     except:
-        print("Call service fail")
+        rospy.loginfo("Call service fail")
+        # rospy.loginfo("test")
     
     rate.sleep()
-    rospy.spin()
+    # rospy.spin()
 
 if __name__=="__main__":
-    rospy.init_node("call_update")
-    call_service()
+    rospy.init_node("call_update", anonymous=True)
+    while not rospy.is_shutdown():
+        call_service()
+        # rospy.loginfo("test3")
+        # rospy.spin()
