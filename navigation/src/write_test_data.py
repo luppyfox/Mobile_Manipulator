06e2b9f50723485f395627a86165d015812526db
phat <luppy.fox@gmail.com>
2024-02-22 10:33:46 -0800

    PID test

--- navigation/src/write_test_data.py
+++ navigation/src/write_test_data.py
@@ -2,11 +2,10 @@
 from math import pi
 import rospy
 from std_msgs.msg import Int64, Float32
-import pandas as pd
 import matplotlib.pyplot as plt
+import pyFileInteraction as pfi
 
-
-class Diff_drive_kinematics():
+class Data_Test():
     def __init__(self):
         self.vl_sub = rospy.Subscriber('/topicV_L', Int64, self.callback_L)
         self.vr_sub = rospy.Subscriber('/topicV_R', Int64, self.callback_R)
@@ -18,9 +17,13 @@ class Diff_drive_kinematics():
         self.vr = 0
         self.rpmL = 8
         self.rpmR = 8
-        self.rate = rospy.Rate(1)
-        self.left_wheel_pub.publish(self.rpmL)
-        self.right_wheel_pub.publish(self.rpmR)
+        self.start_time = rospy.Time.now()
+        self.data_time = []
+        self.data_vl = []
+        self.data_vr = []
+        self.data_all = []
+        self.rate = rospy.Rate(10)
+        
 
     def callback_L(self, msg):
         self.vl = msg.data
@@ -28,29 +31,46 @@ class Diff_drive_kinematics():
     def callback_R(self, msg):
         self.vr = msg.data
 
-    def convert_vels(self):
+    def ploting(self):
+        while not rospy.is_shutdown():
+            self.left_wheel_pub.publish(self.rpmL)
+            self.right_wheel_pub.publish(self.rpmR)
+            self.data_time.append((rospy.Time.now() - self.start_time).to_sec())
+            self.data_vl.append(self.vl)
+            self.data_vr.append(self.vr)
+
+            self.data_all.append([(rospy.Time.now() - self.start_time).to_sec(), self.vl, self.vr])
+            
+            # set
+            # plt.plot(,self.rpmL)
+
+            # # vl
+            # plt.plot(,self.vl, color="red")
         
-# set
-plt.plot(,self.rpmL)
+            # # vr
+            # plt.plot(, self.vr, color='green')
+        
+            # # title
+            # plt.title('PID test')
 
-# vl
-plt.plot(,self.vl)
- 
-# vr
-plt.plot(, self.vr, color='green')
- 
-# title
-plt.title('PID test')
+            # # towards right
+            # plt.xticks(rotation=30, ha='right')
+        
+            # # Giving x and y label to the graph
+            # plt.xlabel('Time')
+            # plt.ylabel('RPM')
+            self.rate.sleep()
+        # self.left_wheel_pub.publish(0)
+        # self.right_wheel_pub.publish(0)
+        plt.plot(self.data_time, self.data_vl, self.data_time, self.data_vr)
+        plt.title('PID test')
+        plt.show()
 
-# towards right
-plt.xticks(rotation=30, ha='right')
- 
-# Giving x and y label to the graph
-plt.xlabel('Date')
-plt.ylabel('Classes')
+        pfi.write_csv(self.data_all, "/home/phat/catkin_ws/src/Mobile_Manipulator/navigation/csv/test_pid.csv")
+        
 
 
 if __name__ == '__main__':
-    rospy.init_node('convert_vel_node', anonymous=True)
-    diffdrive_object = Diff_drive_kinematics()
-    diffdrive_object.pub_wheel_vels()
+    rospy.init_node('test_data_node', anonymous=True)
+    T = Data_Test()
+    T.ploting()
