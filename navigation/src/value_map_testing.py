5809eb2247859428cff830e714024a201d8bac20
Luppy Fox <luppy.fox@gmail.com>
2024-08-10 16:11:31 +0700

    Update value_map_testing.py

--- navigation/src/value_map_testing.py
+++ navigation/src/value_map_testing.py
@@ -1,13 +1,18 @@
 import random
-se = 4.9000
+se = 4.9000 # Standard error that you can reciveable
 #se = 2.9
-goal = -290
-if goal < 0:
+
+goal = -290 # Range of goal value
+
+if goal < 0: # Dir in range
 	dir = -1
 else:
 	dir = 1
+
 min_error = goal - se * dir
 max_error = goal + se * dir
-count = 10
+
+count = 10 # Number of testing
+
 for i in range(0,count):
-	print(random.uniform(min_error, max_error))
+	print(random.uniform(min_error, max_error)) # Random process
